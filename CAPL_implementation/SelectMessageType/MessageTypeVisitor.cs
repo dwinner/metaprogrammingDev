d645bfd31b61e967445f5c49ae070f769af3db6a
vinevtsev <vinevtsev@schleissheimer.de>
2022-06-14 19:02:47 +0200

    Developed Message type visitor as a gathering tool example

---
+++ CAPL_implementation/SelectMessageType/MessageTypeVisitor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CaplGrammar.Core;
+
+namespace SelectMessageType
+{
+    internal class MessageTypeVisitor : CaplBaseVisitor<Dictionary<string, List<string>>>
+    {
+        public Dictionary<string, List<string>> MessageVariables { get; } = new Dictionary<string, List<string>>();
+
+        public override Dictionary<string, List<string>> VisitDeclaration(CaplParser.DeclarationContext context)
+        {
+            var messageType = string.Empty;
+
+            var declSpecCtx = context.declarationSpecifiers()?.declarationSpecifier();
+            if (declSpecCtx != null)
+            {
+                // Find the type specifier in declaration specifier list
+                var typeSpecCtx = declSpecCtx
+                    .Select(specifierCtx => specifierCtx.typeSpecifier())
+                    .FirstOrDefault(typeSpecifierContext => typeSpecifierContext != null);
+
+                // If the type has a message type then it's a message
+                var messageTypeCtx = typeSpecCtx?.messageType();
+                if (messageTypeCtx != null)
+                {
+                    var children = messageTypeCtx.children;
+
+                    /* According to the parser rule messageType in Capl.g4, the 1st child is always keyword
+                       containing alternatives for lexer rules: Message, MultiplexedMessage, MostMessage or MostAmsMessage,
+                       so the other items can gather the raw representation for message ID */
+                    if (children.Count > 1)
+                    {
+                        messageType = children.Skip(1).Aggregate("",
+                            (current, treeItem) => current + treeItem.ToString().Trim());
+                    }
+                }
+            }
+
+            /* If we find the message name under the related parser rule 'declaration'
+               then can get it's message name instances */
+            if (string.IsNullOrEmpty(messageType))
+            {
+                return base.VisitDeclaration(context);
+            }
+
+            if (!MessageVariables.ContainsKey(messageType))
+            {
+                MessageVariables[messageType] = new List<string>();
+            }
+
+            var initDeclCtx = context.initDeclaratorList()?.initDeclarator()
+                              ?? Array.Empty<CaplParser.InitDeclaratorContext>();
+            if (initDeclCtx.Length > 0)
+            {
+                var messageVars = initDeclCtx
+                    .Select(initDecl =>
+                        initDecl.declarator()?.directDeclarator()?.Identifier()?.Symbol.Text ?? string.Empty)
+                    .Where(messageName => !string.IsNullOrEmpty(messageName)).ToList();
+                MessageVariables[messageType].AddRange(messageVars);
+            }
+
+            return base.VisitDeclaration(context);
+        }
+    }
+}
---
+++ CAPL_implementation/SelectMessageType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using Antlr4.Runtime;
+using CaplGrammar.Core;
+
+namespace SelectMessageType
+{
+    internal static class Program
+    {
+        private const string CanFile = "message.can";
+
+        private static void Main()
+        {
+            AntlrInputStream antlrStream = new AntlrFileStream(CanFile, Encoding.UTF8);
+            var caplLexer = new CaplLexer(antlrStream);
+            var tokens = new CommonTokenStream(caplLexer);
+            var caplParser = new CaplParser(tokens) { BuildParseTree = true };
+            var caplAst = caplParser.compilationUnit();
+
+            var messageTypeVisitor = new MessageTypeVisitor();
+            messageTypeVisitor.Visit(caplAst);
+            var messageVariables = messageTypeVisitor.MessageVariables;
+
+            Console.WriteLine();
+        }
+    }
+}
